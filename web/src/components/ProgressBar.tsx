a301a8d3e12d02c730a0c5e045e1bf2172bca9e5
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-01-18 22:52:21 -0300

    Feat: add ProgressBar component

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,18 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props : ProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div 
+        role="progressbar"
+        aria-valuenow={props.progress}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{ width: `${props.progress}%` }}
+      ></div>
+    </div>
+  )
+}
