8b8f6fbd5803c7eb266f28d3da6c5a5f505528d3
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-01-18 23:44:57 -0300

    Feat: create CheckBox component

---
+++ mobile/src/components/CheckBox.tsx
@@ -0,0 +1,35 @@
+import { TouchableOpacity, View,Text, TouchableOpacityProps } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
+
+interface CheckBoxProps extends TouchableOpacityProps {
+  checked?: boolean;
+  title: string;
+}
+
+export function CheckBox({ checked = false, title, ...rest }: CheckBoxProps) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className="flex-row mb-2 items-center"
+      {...rest}
+    >
+      {checked ? (
+        <View className="h-8 w-8 bg-green-500 rounded-lg justify-center items-center">
+        <Feather
+          name="check"
+          size={20}
+          color={colors.white}
+        />
+      </View>
+      ) : (
+        <View className="h-8 w-8 bg-zinc-900" />
+      )}
+
+      <Text className="ml-3 text-white text-base">
+        {title}
+      </Text>
+
+    </TouchableOpacity>
+  )
+}
