5f3a12bc9b1d5983e400ae3424d8f4128c2599ef
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-01-20 10:56:40 -0300

    Feat: integrate NewHabitForm with api

--- mobile/src/screens/NewHabitForm.tsx
+++ mobile/src/screens/NewHabitForm.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import { ScrollView, View,Text, TextInput, TouchableOpacity } from "react-native";
+import { ScrollView, View,Text, TextInput, TouchableOpacity, Alert } from "react-native";
 
 import { Feather } from '@expo/vector-icons'
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
 import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState<string>("")
   const [weekDays, setWeekDays] = useState<number[]>([])
 
   function handleToggleWeekDay(weekDayIndex: number) {
@@ -17,6 +19,25 @@ export function NewHabitForm() {
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex])
     }
+  }
+
+  function handleCreateHabit() {
+    if(!title.trim() || weekDays.length === 0) {
+      Alert.alert("Ops", "Você precisa informar um nome para o hábito e selecionar ao menos um dia da semana")
+    }
+    
+    api.post('/habits', {
+      title,
+      weekDays
+    }).then(() => {
+      Alert.alert("Novo hábito", "Hábito criado com sucesso")
+    }).catch((err) => {
+      Alert.alert("Ops", "Não foi possível criar um novo hábito hábito")
+      console.log(err)
+    }).finally(() => {
+      setTitle("")
+      setWeekDays([])
+    })
   }
 
   return (
@@ -38,6 +59,8 @@ export function NewHabitForm() {
           className="h-12 pl-4 mt-3 text-white bg-zinc-900 rounded-lg border-2 border-zinc-800 focus:border-green-600 "
           placeholderTextColor={colors.zinc[400]}
           placeholder="Ex.: exrcícios, dormir bem etc..."
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="text-base text-white font-semibold mt-4 mb-3">
@@ -56,6 +79,7 @@ export function NewHabitForm() {
         <TouchableOpacity
           className="w-full h-14 flex-row justify-center items-center mt-6 bg-green-600 rounded-lg"
           activeOpacity={0.7}
+          onPress={handleCreateHabit}
         >
           <Feather
             name="check"
