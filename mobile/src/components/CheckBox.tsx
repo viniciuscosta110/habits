dc61270f0efebf6f344b13ee7e2339ed0aab4ba6
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-01-22 16:55:22 -0300

    Feat: add animation

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -1,4 +1,6 @@
 import { TouchableOpacity, View,Text, TouchableOpacityProps } from "react-native";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
+
 import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
 
@@ -15,13 +17,17 @@ export function CheckBox({ checked = false, title, ...rest }: CheckBoxProps) {
       {...rest}
     >
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg justify-center items-center">
+        <Animated.View 
+          className="h-8 w-8 bg-green-500 rounded-lg justify-center items-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
         <Feather
           name="check"
           size={20}
           color={colors.white}
         />
-      </View>
+      </Animated.View>
       ) : (
         <View className="h-8 w-8 bg-zinc-900" />
       )}
--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,16 +1,28 @@
+import { useEffect } from "react";
 import { View } from "react-native"
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated"
 
 interface ProgressBarProps{
   progress?: number;
 }
 
 export function ProgressBar({ progress = 0 }: ProgressBarProps = {}) {
+  const sharedProgress= useSharedValue(progress)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress)
+  }, [progress])
+
   return (
     <View className="w-full h-3 bg-zinc-700 rounded-xl mt-4">
-      <View className="h-3 bg-violet-600 rounded-xl"
-        style={{
-          width: `${progress}%`
-        }}  
+      <Animated.View className="h-3 bg-violet-600 rounded-xl"
+        style={animatedStyle}  
       />
     </View>
   )
