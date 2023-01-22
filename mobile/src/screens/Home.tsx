7ddb123a4ba81a80e779bdb88934d7ae87f3bb7c
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-01-22 16:57:14 -0300

    Feat: update summary on page focus

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,12 +1,12 @@
 import { View, Text, ScrollView, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { api } from "../lib/axios";
 
 import { generateDaysFromYearBeginning } from "../utils/generate-days-from-year-beginning";
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Loading } from "../components/Loading";
 import dayjs from "dayjs";
 
@@ -42,9 +42,9 @@ export function Home() {
     })
   }
 
-  useEffect(() => {
+  useFocusEffect( useCallback(() => {
     fetchData()
-  }, [])
+  }, []))
 
   if(loading) {
     return <Loading />
